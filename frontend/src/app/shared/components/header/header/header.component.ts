311ff54adf45f137d6cf9bf2a66e1b8759edb5f6
Evelyn Matos <evelyn.matos@montreal.com.br>
2025-08-27 23:40:41 -0300

    feat: criação do header

---
+++ frontend/src/app/shared/components/header/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule]
+})
+export class HeaderComponent {
+
+}
