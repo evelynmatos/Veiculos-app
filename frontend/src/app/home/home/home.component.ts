ab195a7c4e906e9d5dfcdb46493a469801aaa6a2
Evelyn Matos <evelyn.matos99@gmail.com>
2025-08-30 17:18:49 -0300

    fix: ajustando pagina home

--- frontend/src/app/home/home/home.component.ts
+++ frontend/src/app/home/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { EmblaCarouselDirective, EmblaCarouselType } from 'embla-carousel-angular';
 
 @Component({
@@ -8,7 +8,7 @@ import { EmblaCarouselDirective, EmblaCarouselType } from 'embla-carousel-angula
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
   @ViewChild(EmblaCarouselDirective) embla!: EmblaCarouselDirective;
   private emblaApi?: EmblaCarouselType;
@@ -26,8 +26,18 @@ export class HomeComponent {
     { img: 'assets/images/vehicle-slide-3.jpg', title: 'Slide 3' },
   ];
 
-  ngAfterViewInit() {
-    this.emblaApi = this.embla.emblaApi;
+ ngAfterViewInit() {
+    setTimeout(() => {
+      this.emblaApi = this.embla?.emblaApi;
+
+      if (this.emblaApi) {
+        setInterval(() => {
+          this.emblaApi?.scrollNext();
+        }, 4000);
+      } else {
+        console.warn('Embla API ainda não carregada.');
+      }
+    });
   }
 
   prev() {
